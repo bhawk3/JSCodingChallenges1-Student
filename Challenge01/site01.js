723dd213b04b1994e60a9ef4d3196d23be22a323
Brendan Hawk <brendanhawk03@aol.com>
2024-12-18 08:44:37 -0500

    Coding Challenge 1 Complete
    The first coding challenge is complete. This commit does not include the extra credit portion of the challenge.

--- Challenge01/site01.js
+++ Challenge01/site01.js
@@ -31,7 +31,22 @@ function findHero() {
 
 //takes an array of strings and returns the longest one. 
 function findLongestString(namesArry) {
+    let longestString = ""
 
-    return "";
+    for (let i = 0; i < namesArry.length; i++) {
+        
+        if(namesArry[i].length > longestString.length){
+            longestString = namesArry[i]
+        }
+    }
+
+    return longestString;
+
 
 }
+
+//create function that takes an array as a parameter
+//declare & initialize a variable to hold the longest string
+//loop over the aray and check if the current string is larger than the longest string
+//if current string is larger set it to the variable holding the longest string
+//return the longest string
